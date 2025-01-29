b67658205b2b100b89f55cc20fac573c2a93386b
Julio <118403153+juliomier@users.noreply.github.com>
2025-01-29 18:00:20 +0000

    Subiendo código desde Codespaces

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-from nv10usb import NV10USB
+import sys
 import time
 
+#agregar tu ruta
+sys.path.append('')
+
+from nv10usb import NV10USB
+
 n = NV10USB(serialport='/dev/ttyACM0')
+
 if not n.INIT_ERROR:
     print('Sincronizando... ', n.sync())
     print('Estableciendo versión 7 del protocolo... ', n.host_protocol_version(7))
@@ -12,14 +18,19 @@ if not n.INIT_ERROR:
     print('Habilitando... ', n.enable())
     print(n.display_on())
 
-    while True:
-        try:
-            poll = n.poll()
-            if len(poll) > 0:
-                print(poll)
-            time.sleep(0.5)
-        except KeyboardInterrupt:
-            print(n.display_off())
-            print('Deshabilitando... ', n.disable())
+    try:
+        while True:
+            try:
+                poll_data = n.poll()
+                if poll_data:  # Verifica que poll_data no sea None ni vacío
+                    print(poll_data)
+                time.sleep(0.5)
+            except Exception as e:
+                print(f"Error durante la lectura del billetero: {e}")
+                time.sleep(1)  # Espera un poco antes de volver a intentar
+    except KeyboardInterrupt:
+        print(n.display_off())
+        print('Deshabilitando... ', n.disable())
+        print("Saliendo del programa.")
 else:
-    print('Error ', n.ERROR)
+    print('Error: ', n.ERROR)
